3898a02542a4004aa083d391dd9de1276cb320d1
valerieniloinsigh31 <107855608+valerieniloinsigh31@users.noreply.github.com>
2023-02-26 17:55:51 +0000

    Add if statement to forloop in todo_list that strikes out item when done

--- todo/views.py
+++ todo/views.py
@@ -5,9 +5,8 @@ from .models import Item
 
 
 def get_todo_list(request):
-    items = Item.object.all()
+    items = Item.objects.all()
     context = {
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
-    
