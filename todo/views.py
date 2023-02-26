12f7cd8bdd5a6aefa705af28a1da45512c13f46d
valerieniloinsigh31 <107855608+valerieniloinsigh31@users.noreply.github.com>
2023-02-26 22:15:46 +0000

    Add edit_item function to views.py

--- todo/views.py
+++ todo/views.py
@@ -23,3 +23,16 @@ def add_item(request):
         'form': form
     }
     return render(request, 'todo/add_item.html', context)
+
+    def edit_item(request):
+    if request.method == 'POST' :
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'todo/add_item.html', context)
+
