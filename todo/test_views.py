e82253fae9ccc56db013f7f9e5781dc4aa1f015d
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-02-27 20:37:03 +0000

    Add test to test_views

--- todo/test_views.py
+++ todo/test_views.py
@@ -4,16 +4,29 @@ from django.test import TestCase
 
 class TestDjango(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1,1)
+    def test_can_get_todo_list(self):
+        
 
-    def test_this_thing_works2(self):
-        self.assertEqual(1,3)
+    def test_get_additem_page(self):
+        
 
-    def test_this_thing_works3(self):
-        self.assertEqual(1,)
+    def test_get_edit_item_page(self):
+        
 
-    def test_this_thing_works4(self):
-        self.assertEqual(1,4)
+    def test_can_add_item(self):
 
-    def test_can_edit_item():
+    def test_can_delete_item(self):
+
+    def test_can_toggle_item(self):
+        
+
+
+
+
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test ToDo Item')
+        response=self.client.post(f'/edit/{item.id}', {'name' : 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=items.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
