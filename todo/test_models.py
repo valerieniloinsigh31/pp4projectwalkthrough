141051713a65b0e213902e565fddd87cc72f4457
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-02-27 19:59:55 +0000

    Coverage-to show code actually tested

---
+++ todo/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from .models import Item 
+
+
+class TestModels(TestCase):
+
+
+    def test_done_defaults_to_false(self):
+        item = Item.objects.create(name = 'Test ToDo Item')
+        self.assertFalse(item.done)
