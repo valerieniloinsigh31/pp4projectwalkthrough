a610d40de67406f0318c7e455a65eb857ba5bdd7
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-02-28 20:21:26 +0000

    Add test views

--- todo/test_views.py
+++ todo/test_views.py
@@ -5,24 +5,24 @@ from django.test import TestCase
 class TestDjango(TestCase):
 
     def test_can_get_todo_list(self):
-        
+        response=self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
 
     def test_get_additem_page(self):
         
 
-    def test_get_edit_item_page(self):
-        
+    def test_get_edit_item_page(self): 
+
 
     def test_can_add_item(self):
 
+
     def test_can_delete_item(self):
+
 
     def test_can_toggle_item(self):
         
-
-
-
-
 
     def test_can_edit_item(self):
         item = Item.objects.create(name='Test ToDo Item')
