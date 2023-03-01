84f9d847c50fc434b8ab17442d40a9df4436b518
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-01 20:45:41 +0000

    Need to add env.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -74,11 +74,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATBASE_URL'))
 }
 
 
