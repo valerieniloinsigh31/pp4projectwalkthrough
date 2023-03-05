3298aa218359349d86787d5c564e9fd9715002a7
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-05 16:02:54 +0000

    Prepare to deploy to Heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,11 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+
+if os.path.isfile("env.py"):
+    import env
 
 from pathlib import Path
 import os
 import dj_database_url
-import env
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
