da896d969699d677ceb21b1aeafd438d233bba51
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-05 17:01:17 +0000

    Add app to allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-gw&&n2$v^x(uab7q2xmt$^_*t+@)wy6ec_cihm(u)urw0*8c^!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangopenultimateproject.herokuapp.com']
 
 
 # Application definition
