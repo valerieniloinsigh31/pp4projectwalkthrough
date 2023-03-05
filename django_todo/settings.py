14e58b90578ec43e964cbd75e4d05f69ad870db7
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-03-05 17:52:59 +0000

    Set up development environment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-gw&&n2$v^x(uab7q2xmt$^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
